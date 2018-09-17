84cbfae29fa3e03d4a7b8b813b22d83c3a8ceae7
Eric Darsow <edarsow@andrew.cmu.edu>
2018-09-17 08:37:32 -0400

    first commit; basic class hierarchy example

---
+++ javaoo_netbeans/src/classdiagrams/patternprinting/PrinterLand.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classdiagrams.patternprinting;
+
+/**
+ * Demo client class which instantiates objects
+ * who are part of a multi-class structure.
+ * @author delores
+ */
+public class PrinterLand {
+    public static void main(String[] args) {
+        BoxPrinter bp = new LoopingBoxer();
+        bp.PrintABox(5);
+    }
+    
+}
