2e23aaa974fced0be60dc3b63961d15bbc5c5235
Darsow <edarsow@ccac.edu>
2019-03-20 14:54:27 -0400

    MVC sample

---
+++ javaoo_netbeans/src/gui/FileModel.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author edarsow
+ */
+public class FileModel {
+
+    public void writeFile(String fileName) {
+        PrintWriter fileWriter = null;
+        StringBuilder sb = new StringBuilder();
+        sb.append("src/files/");
+        sb.append(fileName);
+
+        try {
+            java.io.File nameFile = new java.io.File(sb.toString());
+            System.out.println("Writing to: " + nameFile.getAbsolutePath());
+            // connect our writer object with the file to write to
+            fileWriter = new PrintWriter(nameFile);
+            fileWriter.print("Mr. Ri+zvi\n");
+            fileWriter.print("Mr.Eged");
+            
+            fileWriter.flush();
+            fileWriter.close();
+
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex);
+        } finally {
+            if (fileWriter != null) {
+                fileWriter.close();
+            }
+        }
+    }
+
+    public String readFile(String fn) {
+        StringBuilder sb = new StringBuilder();
+        String fname = "src/files/" + fn;
+        Scanner fileScanner = null;
+        try {
+            fileScanner = new Scanner(new File(fname));
+//        fileScanner.useDelimiter("\n");
+//        System.out.println("Delim: " + fileScanner.delimiter());
+        } catch (FileNotFoundException ex) {
+            System.out.println("File not found");
+        }
+
+        while(fileScanner.hasNext()){
+            sb.append(fileScanner.next());
+            
+            
+        }
+//        sb.append(fileScanner.next());
+//        sb.append(fileScanner.next());
+//        sb.append(fileScanner.next());
+        System.out.println("File Contents: " + sb.toString());
+        return sb.toString();
+    }
+
+}
---
+++ javaoo_netbeans/src/gui/FileView.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author edarsow
+ */
+public class FileView {
+    
+    
+    private FileController controller;
+    private JLabel output;
+    
+    
+    public void initializeWindow(){
+        JFrame jf = new JFrame("Simple File");
+        jf.setLayout(new FlowLayout(FlowLayout.LEFT, 10, 20));
+        
+        JButton loadButton = new JButton("Load file data");
+        loadButton.setSize(80, 50);
+        loadButton.addActionListener(new LoadFileDataListener());
+        
+        JTextArea textDisplay = new JTextArea("Area");
+        textDisplay.setSize(50, 80);
+        
+        JTextField fileDataField = new JTextField("");
+        fileDataField.setSize(80, 50);
+        
+        output = new JLabel("");
+        
+        
+        jf.add(fileDataField);
+        jf.add(loadButton);
+        jf.add(textDisplay);
+        jf.add(output);
+        
+        
+        jf.setSize(400, 400);
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setLocationRelativeTo(null);
+        jf.setVisible(true);
+        
+    }
+    
+    class LoadFileDataListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if(controller != null){
+                String t = controller.retrieveFileContents(FileController.FILE_NAME);
+                System.out.println("Inside Action Listener; file: " + t);
+                output.setText(t);
+            }
+        }
+    }
+
+    /**
+     * @return the controller
+     */
+    public FileController getController() {
+        return controller;
+    }
+
+    /**
+     * @param controller the controller to set
+     */
+    public void setController(FileController controller) {
+        this.controller = controller;
+    }
+    
+}
