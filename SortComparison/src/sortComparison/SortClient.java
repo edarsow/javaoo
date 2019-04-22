391818362f036c05c6490cf3f57ff7dab1d4ccc8
efaux01 <eafaux@gmail.com>
2019-04-22 15:09:38 -0400

    Finished SortComparis:on

--- SortComparison/src/sortComparison/SortClient.java
+++ SortComparison/src/sortComparison/SortClient.java
@@ -6,6 +6,7 @@
 package sortComparison;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  *
@@ -16,11 +17,52 @@ public class SortClient {
     
     
     public static void main(String args[]){
-        Sorter sort = new Sorter();
         
+        runSystem();
     }
     
-    public static void 
+    public static void runSystem(){
+        Sorter sort = new Sorter();
+        System.out.println("How long of a list do you want to sort?");
+        Scanner scan = new Scanner(System.in);
+        int arrSize = scan.nextInt();
+        int[] arr1 = getRand(arrSize);
+        int[] arr2 = arr1;
+        
+        int[] sort1 = sort.sortMeth(arr1);
+        int[] sort2 = sort.sort2Meth(arr2);
+        
+        System.out.println("Bubble Sort: ");
+        //printArray(sort1);
+        System.out.println("Array is Sorted: " + checkArr(sort1));
+        System.out.println("Sort time: " + subtract(sort.runTimes1) + " milliseconds");
+        
+        System.out.println("\nQuicksort: ");
+        //printArray(sort2);
+        System.out.println("Array is sorted: " + checkArr(sort2));
+        System.out.println("Sort time: " + subtract(sort.runTimes2) + " milliseconds");
+        
+        runAgain();
+    }
+    
+    public static void runAgain(){
+        int again = 0;
+        while (again != 1 && again !=2){
+            System.out.println("Would you like to test the sort algorithms again? \n1-yes \n2-no");
+            Scanner scan = new Scanner(System.in);
+            again = scan.nextInt();
+            switch(again){
+                case 1: 
+                    runSystem();
+                    break;
+                case 2: 
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice, try again.");
+            }
+        }
+    }
     
     public static int[] getRand(int n){
         int[] list = new int[n];
@@ -33,7 +75,7 @@ public class SortClient {
     
     public static boolean checkArr(int[] ar){
         boolean isSorted = true;
-        for(int i = 1; i < ar.length && isSorted; i++){
+        for(int i = 0; i < ar.length - 1 && isSorted; i++){
             if (ar[i] > ar[i + 1]){
                 isSorted = false;
             }
@@ -49,6 +91,7 @@ public class SortClient {
     }
     
     public static long subtract(long[] sub){
+        //System.out.println("Time 1: " + sub[0] + "Time 2: " + sub[1]);
         long ans = sub[1]-sub[0];
         return ans;
     }
