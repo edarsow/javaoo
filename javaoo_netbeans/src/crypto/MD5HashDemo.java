9e10154f47861efed2320bbc7e841af1e1541841
Darsow <edarsow@ccac.edu>
2019-10-14 14:02:15 -0400

    added Hash Sample

--- javaoo_netbeans/src/crypto/MD5HashDemo.java
+++ javaoo_netbeans/src/crypto/MD5HashDemo.java
@@ -5,40 +5,35 @@
  */
 package crypto;
 
-import com.sun.org.apache.xml.internal.security.exceptions.Base64DecodingException;
-import com.sun.org.apache.xml.internal.security.utils.Base64;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.xml.bind.DatatypeConverter;
 
 /**
  *
  * @author edarsow
  */
-public class Md5Test {
+public class MD5HashDemo {
     
-    private static final String importantMessage = "Acceleration is nine point eight "
+    private static final String importantMessage = "Acceleratuon is nine point eight "
             + "meters NOT SECOND per second on earth";
     
+    /**
+     * Demo method for computing an MD5 hash value of an incoming String
+     * @param args 
+     */
     public static void main(String[] args) {
         System.out.print("INPUT: ");
         System.out.println(importantMessage);
         try {
             MessageDigest digester = MessageDigest.getInstance("SHA-256");
             digester.update(importantMessage.getBytes());
-            byte[] hashValue = digester.digest();
-            
-            StringBuffer hexbuffer = new StringBuffer();
-            for (int i = 0; i < hashValue.length; i++){
-                hexbuffer.append(Integer.toHexString(0xFF & hashValue[i]));
-            }
-            
-            System.out.print("HASH: ");
-            System.out.println(hexbuffer.toString());
+            String h = DatatypeConverter.printHexBinary(digester.digest());
+            System.out.println("hashed value: " + h);
         } catch (NoSuchAlgorithmException ex) {
             System.out.println(ex);
         }
     }
-    
 }
