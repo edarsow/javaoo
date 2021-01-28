30bb86ad9d2a9c0cbf6bf02c33bdd4d3c1dd2386
Eric Darsow <edarsow@ccac.edu>
2021-01-28 09:33:56 -0500

    TODO in small group

--- javaoo_netbeans/src/arrays/CollectionLand.java
+++ javaoo_netbeans/src/arrays/CollectionLand.java
@@ -15,6 +15,22 @@ public class CollectionLand {
      * @param args 
      */
     public static void main(String[] args) {
+        // When possible, do as little coding in main
+        // as practical, to help with code re-use
+        
+        // Turn off method to use user input
+//        loadSampleCollectionData();
+
+        startUserInputtedCollection();
+        
+        
+    } // close main
+    
+    /**
+     * Test code for creating and using our CollecetedItems
+     * class
+     */
+    public static void loadSampleCollectionData(){
         // Make an instance of my CollectedItems
         CollectedItems elecTools = new CollectedItems();
         // called our setup method, passing in 10 for numItems
@@ -36,9 +52,33 @@ public class CollectionLand {
         CollectedItems popSongs = new CollectedItems();
         // Keep going!
         
-        
-    } // close main
+    } // close loadSampleData
     
+    /**
+     * Method which allows users to add an item to a collection
+     * and view its output
+     */
+    public static void startUserInputtedCollection(){
+        System.out.println("Welcome to the collection tool");
+        System.out.println("How many items do you want to collect?");
+        // TODO: Create a Scanner instance
+        // TODO: use Scanner to read an integer from user
+        // Also ask for the name of the collection
+        
+        // TODO: Feed the user's num & name to our setupCollection method
+        
+        // TODO: Ask user to enter values for each item in 
+        // the collection
+        
+        // Display all collected items
+        // OPTIONAL: Use a switch statement to
+        // create a menu like interface
+        // EG: 1: Start new collecetion
+        //     2: Enter items
+        //     3: Display items
+        //     4: Remove, reorder, rename items?
+        
+    }
     
     
     
